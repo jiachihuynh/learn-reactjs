201147c979b6905feae4d0306a1df49e4e6d325c
jiachihuynh <jaja.huynh@gmail.com>
2019-06-01 21:28:10 +0700

    Fix UI

--- todo-list/src/components/todoitem.js
+++ todo-list/src/components/todoitem.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 import './todoitem.css';
 import classNames from 'classnames';
+import checkImg from '../img/check.svg';
+import checkCompleteImg from '../img/check-complete.svg';
 
 class TodoItem extends Component { 
     render() {
         const {item, onClick} = this.props;
+        let url = checkImg;
+        if (item.isComplete) {
+            url = checkCompleteImg;
+        }
 
         return (
-            <div onClick={onClick} className={classNames('TodoItem', {
+            <div  className={classNames('TodoItem', {
                 'TodoItem-complete': item.isComplete
             })}>
+              <img 
+                src={url} 
+                width={32} 
+                height={32}
+                onClick={onClick} />
               <p>{this.props.item.title}</p>
             </div> 
         )
