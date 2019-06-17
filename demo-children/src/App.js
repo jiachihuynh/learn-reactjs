2d56c9ac5846e98fdbcb3f23bad7f2659d30ee78
jiachihuynh <jaja.huynh@gmail.com>
2019-06-17 22:12:29 +0700

    'Add stage'

--- demo-children/src/App.js
+++ demo-children/src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
+import Accordion from './components/accordion';
 
 class App extends Components {
   render(){
     return (
       <div className="App">
-
+        <Accordion headig="Heading" content="Hello Jiachi"/>
       </div>
     );
   }
---
+++ demo-children/src/components/accordion.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class Accordion extends Components {
+    constructor(props){
+        super(props);
+        this.state = {
+            isCollapsed: true
+        };
+    }
+
+    render() {
+      const { heading, content } = this.props;
+      const { isCollapsed } = this.state;
+
+      return <div className="Accordion">
+        <div className="Heading">
+        <h2>{ heading }</h2>
+        </div>
+        <div className="Content">{ content }</div>
+      </div>
+    }
+}
+
+export default Accordion;
