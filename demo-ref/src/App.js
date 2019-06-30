ca79b96405ebde5be6351457d42567795a131ac5
jiachihuynh <jaja.huynh@gmail.com>
2019-06-30 22:00:39 +0700

    Complete demo-ref

--- demo-ref/src/App.js
+++ demo-ref/src/App.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElement = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElement.current.focus();
+  }
+
   render() {
     return (
       <div className="App">
-        <input type="text"/>
+        <input type="text" ref={this.inputElement}/>
       </div>
     );
   }
